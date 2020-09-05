75132e1097430fbf25f198a7c3c04a642a763052
crunchbone <67559704+crunchbone@users.noreply.github.com>
2020-09-05 15:31:48 -0500

    Create math-module-test.lua

---
+++ math-module-test.lua
@@ -0,0 +1,17 @@
+local math = {}
+
+local function math.add(x,y)
+return x+y
+end
+
+local function math.subtract(x,y)
+return x-y
+end
+
+local function math.multipy(x,y)
+return x*y
+end
+
+local function math.divide(x,y)
+return x/y
+end
