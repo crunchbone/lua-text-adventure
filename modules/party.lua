c4e863889d14aafb118b77ce9bfc326fa11dea28
crunchbone <bilverbone@gmail.com>
2020-09-07 20:41:22 +0000

    updated party.lua

--- modules/party.lua
+++ modules/party.lua
@@ -1,6 +1,19 @@
 local party = {}
-
-function party.NewMember()
+--[[everyone needs:
+  -name
+  -xp
+  -hp
+  -lvl
+]]--
+function party.NewMain(x)
+  local party.Main 
+  party.Main.name = x
+  party.Main.hp = 100
+  party.Main.xp = 0
+  party.Main.lvl = 0
+  print("main char created!")
+end
+function party.NewMember(x)
   print("added new member!")
 end
 
